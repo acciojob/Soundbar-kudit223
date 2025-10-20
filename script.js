80e7ab1a6d95f3a6d5967e0e02ecd6fe92a9fa79
Udit Kumar <58100192+kudit223@users.noreply.github.com>
2025-10-20 17:11:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+let sounds = [
+  "https://kudit223.github.io/sounds/applause-108368.mp3",
+  "https://kudit223.github.io/sounds/boo-6377.mp3",
+  "https://kudit223.github.io/sounds/gasp-6253.mp3",
+  "https://kudit223.github.io/sounds/toda.mp3",
+  "https://kudit223.github.io/sounds/victory-chime-366449.mp3",
+  "https://kudit223.github.io/sounds/wrong-47985.mp3"
+];
+
+let buttons = document.querySelectorAll('.btn');
+let stop = document.querySelector('.stop');
+
+buttons.forEach((button, i) => {
+  let audio = new Audio(sounds[i]);
+  button.addEventListener('click', (e) => {
+    audio.play()
+  });
+  stop.addEventListener('click', (e)=>audio.pause());
+});
